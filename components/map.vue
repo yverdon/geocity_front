4a76a124f46e42a7a840554ac76b220245b821b3
Monod Olivier <olivier.monod@yverdon-les-bains.ch>
2020-08-28 16:32:21 +0200

    switch whole map to EPSG:2056

--- components/map.vue
+++ components/map.vue
@@ -6,15 +6,17 @@
           :zoom.sync="zoom"
           :center.sync="center"
           :rotation.sync="rotation"
+          :projection.sync="projection"
         ></vl-view>
 
         <vl-layer-tile>
           <vl-source-osm></vl-source-osm>
         </vl-layer-tile>
-
-        <vl-feature>
-          <vl-geom-point :coordinates="[38.726634, 9.003391]"></vl-geom-point>
-        </vl-feature>
+        <vl-layer-vector>
+          <vl-source-vector
+            url="http://localhost:9095/rest/events/?starts_at=2020-01-01&ends_at=2020-12-30&adminentity=1"
+          ></vl-source-vector>
+        </vl-layer-vector>
       </vl-map>
     </div>
   </div>
@@ -24,9 +26,10 @@
 export default {
   data() {
     return {
-      zoom: 15,
+      zoom: 10,
       center: [2538888, 1181125],
       rotation: 0,
+      projection: 'EPSG:2056',
     }
   },
 }
--- plugins/vuelayers.js
+++ plugins/vuelayers.js
@@ -3,10 +3,22 @@ import VueLayers from 'vuelayers'
 import 'vuelayers/lib/style.css'
 import proj4 from 'proj4'
 import { register } from 'ol/proj/proj4'
+import { createProj, addProj } from 'vuelayers/lib/ol-ext'
+
 proj4.defs(
   'EPSG:2056',
   '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs'
 )
 register(proj4)
 
-Vue.use(VueLayers)
+const swissGrid = createProj({
+  code: 'EPSG:2056',
+  extent: [2485071.58, 1075346.31, 2828515.82, 1299941.79],
+  units: 'm',
+})
+
+addProj(swissGrid)
+
+Vue.use(VueLayers, {
+  dataProjection: 'EPSG:2056',
+})
