f94649907ef9e14fca49665ac338e00b534dea93
ldgt <ludovic.turmel@liip.ch>
2020-11-17 14:24:57 +0100

    Change Introduction button style

--- components/layers/Introduction.vue
+++ components/layers/Introduction.vue
@@ -8,7 +8,7 @@
       class="flex flex-col items-center border-t-1 md:border-t-0 md:border-l-1 border-brand pt-6 md:pt-0 md:pl-12"
     >
       <p>{{ $t('ask') }}</p>
-      <a href="#" class="btn btn--primary mt-12">{{ $t('askcta') }}</a>
+      <a href="#" class="btn btn--default mt-12">{{ $t('askcta') }}</a>
     </div>
   </div>
 </template>
